289bac42163fa672a97dc63668ecc7b4f00bb98a
Michael Hayes <michael.hayes@marcellin.school.nz>
2025-09-01 13:17:58 +1200

    Modifications of Wire Node
    Experimenting to make a wire cloned from a set scene upon the pairs that are made with the coupled nodes

--- scripts/line_2d.gd
+++ scripts/line_2d.gd
@@ -1,4 +1,5 @@
-extends Line2D
+extends creation
+@onready var line_2d: Line2D = $"."
 
 @onready var wire_node: ColorRect = $"../Wire_Node"
 @onready var wire_node_2: ColorRect = $"../Wire_Node2"
@@ -9,11 +10,20 @@ var right_side := Vector2.ZERO
 func _ready() -> void:
 	left_side = wire_node.size
 	right_side = wire_node_2.size
-	add_point(left_side, 0)
-	add_point(right_side, 1)
+	#add_point(left_side, 0)
+	#add_point(right_side, 1)
+	print(connections)
 
 func _physics_process(delta: float) -> void:
 	left_side = (wire_node.size / 2 + wire_node.position)
 	right_side = (wire_node_2.size / 2 + wire_node_2.position)
-	set_point_position(0, left_side)
-	set_point_position(1, right_side)
+	line_2d.set_point_position(0, left_side)
+	line_2d.set_point_position(1, right_side)
+	
+	#for area in wire_collision.get_overlapping_areas():
+			#if area.name == "Power_Collision":
+				#default_color = Color.RED
+			#elif area.name == "Wire_Collision":
+				#default_color = Color.YELLOW_GREEN
+			#else:
+				#default_color = Color.WHITE
--- scripts/wire_node.gd
+++ scripts/wire_node.gd
@@ -1,25 +1,16 @@
 extends Components
 
 @onready var wire_collision: Area2D = $Wire_Collision
-@onready var line_2d: Line2D = $"../Line2D"
+@onready var _line_scene = preload("res://scenes/test.tscn")
+@onready var line_2d: Line2D = $Line2D
 
 func _physics_process(delta: float) -> void:
 	if not wire_collision.get_overlapping_areas() and not draggable:
 		inside = false
-		position = last_pos
+		global_position = last_pos
 	else:
 		inside = true
 		
-	for area in wire_collision.get_overlapping_areas():
-		if area.name == "Power_Collision":
-			if powered:
-				line_2d.default_color = Color.RED
-		elif area.name == "Wire_Collision":
-			if powered:
-				line_2d.default_color = Color.YELLOW_GREEN
-		#else:
-			#line_2d.default_color = Color.WHITE
-
 func _input(event) -> void:
 	if event is InputEventMouseButton:
 		if mouse_in and event.is_pressed() and inside:
@@ -27,4 +18,4 @@ func _input(event) -> void:
 		elif event.is_released() and mouse_in:
 			draggable = false
 			if wire_collision.get_overlapping_areas():
-				last_pos = position
+				last_pos = global_position
