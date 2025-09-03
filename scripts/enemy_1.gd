947b7b8999310e16d7d8b1c9653a275828a7b20b
Michael Hayes <michael.hayes@marcellin.school.nz>
2025-09-03 21:08:05 +1200

    Shooting Enemies
    Added a shader that detects when it is collided with a bullet that simulates a blink like effect on the enemy

---
+++ scripts/enemy_1.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func apply():
+	var tween = get_tree().create_tween()
+	tween.tween_method(set_shader, 1.0, 0.0, 0.5)
+
+func set_shader(new_Value: float):
+	sprite_2d.material.set_shader_parameter("blink_intensity", new_Value)
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "Bullet":
+		print(sprite_2d.global_position)
+		apply()
--- scripts/gun.gd
+++ scripts/gun.gd
@@ -4,18 +4,23 @@ var _bullet_scene = preload("res://scenes/bullet.tscn")
 
 var mouse_pos : Vector2
 var idk : int
-var timer : bool
-
-func _physics_process(delta: float) -> void:
-	mouse_pos = get_global_mouse_position()
-	look_at(Vector2(mouse_pos.x, mouse_pos.y))
-	
-	if Input.is_action_pressed("click_left"):
-		fire()
-		
+var shootable := true
 
 func fire():
 	var bullet = _bullet_scene.instantiate()
 	bullet.position = $Marker2D.global_position
 	bullet.direction = $Marker2D.global_position - global_position
 	get_tree().get_root().add_child(bullet)
+
+func _physics_process(delta: float) -> void:
+	mouse_pos = get_global_mouse_position()
+	look_at(Vector2(mouse_pos.x, mouse_pos.y))
+	
+	if Input.is_action_pressed("click_left") and shootable:
+		if !mouse_pos.x > 216:
+			$Timer.start()
+			fire()
+			shootable = false
+
+func _on_timer_timeout() -> void:
+	shootable = true
